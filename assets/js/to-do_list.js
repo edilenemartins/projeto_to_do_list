5426af6e7a2c7eaf9d2db21ef31484f905853ecc
edilenemartins <edilenecmartins@yahoo.com.br>
2022-06-06 10:07:40 -0300

    melhorando o projeto

--- assets/js/to-do_list.js
+++ assets/js/to-do_list.js
@@ -9,12 +9,29 @@ function adicionaTarefa() {
     // pegar o valor informado no input. 
     var texto = document.getElementById('texto').value;
 
-    // pegar o conteúdo do elemento que irá guardar os valores informados.
-    let lista_tarefas = document.getElementById('tasks').innerHTML;
+    // pegar a div para inserir a lista de tarefas
+    var lista = document.getElementById('tasks');
 
-    // acrescenta à lista o novo valor sem perder o que já foi informado.
-    lista_tarefas += "<input type='checkbox' id='checkbox'></input><label for='checkbox'>" + texto + "</label>" + "<br>";
+    // criando o novo elemento na página (um input checkbox + a label com o valor digitado)
+    const lista_tarefas = document.createElement('div'); // cria uma nova div para colocar cada tarefa nova
+    const novaTarefa = document.createElement('input');
+	const novoLabel = document.createElement('label');
+	const novaDescricao = document.createTextNode(texto); // insere o valor informado no input na nova tarefa
 
-    // atualiza o campo que guarda as tarefas informadas com os novos valores.
-    document.getElementById('tasks').innerHTML = lista_tarefas;    
+    // cria o tipo checkbox e coloca um id para o input criado acima.
+    novaTarefa.setAttribute('type', 'checkbox');
+    novaTarefa.setAttribute('id', texto);
+
+    // cria o atributo da label criada acima referenciando ao input através do for 
+    // e adiciona a nova descrição de tarefa ao novo label.
+    novoLabel.setAttribute('for', texto); // for quer dizer que referencia ao input acima do checkbox.
+    novoLabel.appendChild(novaDescricao); 
+    
+    // adiciona à div criada no index.html
+    lista_tarefas.classList.add('task-item');
+    lista_tarefas.appendChild(novaTarefa);
+    lista_tarefas.appendChild(novoLabel);
+
+    // dentro do div tasks do index.html adiciona a minha nova tarefa 
+    lista.appendChild(lista_tarefas);
 }
